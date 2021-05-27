6de1fe8c9b44a62db60f0b20ff67c2e61abeb18d
Koussaïla Ben Mamar <kous92@gmail.com>
2021-05-27 18:32:50 +0200

    Ajout premiers tests UI

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ Projet personnel en développement iOS. Application iOS native de news en temps 
 - Téléchargement et récupération de news locales d'un pays par le biais d'une API REST
 - Carte des news par pays.
 - Paramètres de localisation GPS, de news locales favorites (automatique avec le GPS ou manuelle)
+- Architecture MVC
+
+## Frameworks
+
+Frameworks officiels:
+- UIKit
+- Foundation
+- MapKit
+- Network
+- SafariServices
+
+Frameworks tiers:
+- Alamofire
+- Kingfisher
+
+## Tests unitaires et UI
+
+Utilise XCTest, couverture actuelle du code: **18,3%**
+- Tests unitaires classiques et asynchrones.
+- Tests UI classiques et asynchones.
--- SuperNews/SuperNewsUITests/SuperNewsUITests.swift
+++ SuperNews/SuperNewsUITests/SuperNewsUITests.swift
@@ -7,8 +7,13 @@
 
 import XCTest
 
+// UI tests must launch the application that they test.
+// Use recording to get started writing UI tests.
+// Use XCTAssert and related functions to verify your tests produce the correct results.
 class SuperNewsUITests: XCTestCase {
-
+    
+    // Les tests UI doivent lancer l'application qu'ils testent avec app.launch().
+    var app: XCUIApplication!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -23,14 +28,50 @@ class SuperNewsUITests: XCTestCase {
     }
 
     func testHome() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-                // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.tabBars.buttons["Accueil"].tap()
+        
+        XCTAssert(app.staticTexts["Bienvenue"].exists)
     }
-
+    
+    // En cliquant sur "News", on s'assure que les cellules apparaissent. Test de type asynchrone (étant donné le téléchargement asynchrone des données).
+    func testNews() throws {
+        app = XCUIApplication()
+        app.launch()
+        app.tabBars.buttons["News"].tap()
+        
+        // XCTAssert(app.staticTexts["Bienvenue"].exists)
+        XCTAssert(app.images["magnifyingglass"].exists, "Le logo de recherche (loupe) existe.")
+        XCTAssert(app.textFields["newsSearchBar"].exists, "La barre de recherche existe.")
+        
+        // Le TableView existe
+        let articleTableView = app.tables["articleTableView"]
+        XCTAssertTrue(articleTableView.exists, "Le TableView des articles existe")
+        
+        // On vérifie l'existence des cellules
+        let tableCells = articleTableView.cells
+        
+        if tableCells.count > 0 {
+            let promise = expectation(description: "En attente des TableViewCells")
+            
+            for i in 0 ..< tableCells.count {
+                // Grab the first cell and verify that it exists and tap it
+                let tableCell = tableCells.element(boundBy: i)
+                XCTAssertTrue(tableCell.exists, "La cellule \(i) existe")
+         
+                if i == (tableCells.count - 1) {
+                    promise.fulfill()
+                }
+            }
+            waitForExpectations(timeout: 40, handler: nil)
+            XCTAssertTrue(true, "Finished validating the table cells")
+         
+        } else {
+            XCTAssert(false, "Was not able to find any table cells")
+        }
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
